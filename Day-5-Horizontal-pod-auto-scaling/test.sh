2301b5475eeca7b79b2659542fb8dc50dce22a1a
sameershaik <99790327+sameershaik6@users.noreply.github.com>
2026-09-02 13:00:43 +0530

    Update curl command to use new ELB endpoint

--- Day-5-Horizontal-pod-auto-scaling/test.sh
+++ Day-5-Horizontal-pod-auto-scaling/test.sh
@@ -1,6 +1,6 @@
 for j in {1..10}; do  
     for i in {1..1000}; do  
-        curl -s -o /dev/null -w "%{http_code}\n" http://a3d36bd399d15400f8e03cbaaa3f46c9-163245138.us-east-1.elb.amazonaws.com/ &  
+        curl -s -o /dev/null -w "%{http_code}\n" http://a2f85a40a8c094358b95a05c50dfb0ef-505311935.us-east-1.elb.amazonaws.com/ &  
     done  
     wait  # Wait for all background curl processes to finish before next iteration
 done
